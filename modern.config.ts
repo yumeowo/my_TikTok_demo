f697b11e414d67cd67df92610b4974deedea023d
Li WenLin <yume101099@gmail.com>
2025-11-20 11:45:38 +0800

    feat: add tailwindcss and semi-ui dependencies

--- modern.config.ts
+++ modern.config.ts
@@ -1,4 +1,5 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
+import { tailwindcssPlugin } from '@modern-js/plugin-tailwindcss';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
     appTools({
       bundler: 'rspack', // Set to 'webpack' to enable webpack
     }),
+    tailwindcssPlugin(),
   ],
 });
